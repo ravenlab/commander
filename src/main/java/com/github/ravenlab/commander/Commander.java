ffbc024b63a7017674ebcf3b5b0232b5b1597724
virustotalop <virustotalop@gmail.com>
2020-05-02 19:52:08 -0700

    Make CommanderBuilder accept CommanderCommand

--- src/main/java/com/github/ravenlab/commander/Commander.java
+++ src/main/java/com/github/ravenlab/commander/Commander.java
@@ -5,6 +5,6 @@ import com.github.ravenlab.commander.command.CommanderCommand;
 public class Commander {
 
 	public CommanderBuilder create(CommanderCommand command) {
-		
+		return new CommanderBuilder(command);
 	}
 }
--- src/main/java/com/github/ravenlab/commander/CommanderBuilder.java
+++ src/main/java/com/github/ravenlab/commander/CommanderBuilder.java
@@ -4,6 +4,11 @@ import com.github.ravenlab.commander.command.CommanderCommand;
 
 public class CommanderBuilder {
 
+	private CommanderCommand parent;
+	protected CommanderBuilder(CommanderCommand parent) {
+		this.parent = parent;
+	}
+	
 	public CommanderBuilder addChild(CommanderCommand child) {
 		
 	}
