db5dae5003a8992f671fcb4d3b0fc5173069a19d
virustotalop <virustotalop@gmail.com>
2020-05-09 05:55:03 -0700

    Remove CommanderPlayer getNative()

--- src/main/java/com/github/ravenlab/commander/player/CommanderPlayer.java
+++ src/main/java/com/github/ravenlab/commander/player/CommanderPlayer.java
@@ -5,17 +5,10 @@ import java.util.UUID;
 import com.github.ravenlab.commander.sender.CommanderSender;
 
 public abstract class CommanderPlayer<T> extends CommanderSender<T> {
-
-	private T player;
 	
 	public CommanderPlayer(T player) {
 		super(player);
 	}
 	
 	public abstract UUID getUniqueId();
-	
-	@Override
-	public T getNative() {
-		return this.player;
-	}
 }
