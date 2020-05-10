c827f2311be22463306745b4054d667f53f932c6
virustotalop <virustotalop@gmail.com>
2020-05-09 19:27:37 -0700

    Parse arguments in BukkitCommandWrapper

--- src/main/java/com/github/ravenlab/commander/command/bukkit/BukkitCommandWrapper.java
+++ src/main/java/com/github/ravenlab/commander/command/bukkit/BukkitCommandWrapper.java
@@ -9,6 +9,8 @@ import org.bukkit.entity.Player;
 import com.github.ravenlab.commander.command.CommandArgs;
 import com.github.ravenlab.commander.command.CommandData;
 import com.github.ravenlab.commander.command.CommanderCommand;
+import com.github.ravenlab.commander.command.parser.CommandParser;
+import com.github.ravenlab.commander.command.parser.ParserData;
 import com.github.ravenlab.commander.player.bukkit.BukkitCommanderPlayer;
 import com.github.ravenlab.commander.sender.CommanderSender;
 import com.github.ravenlab.commander.sender.bukkit.BukkitCommanderSender;
@@ -16,10 +18,12 @@ import com.github.ravenlab.commander.sender.bukkit.BukkitCommanderSender;
 public class BukkitCommandWrapper extends Command {
 
 	private CommanderCommand command;
+	private CommandParser parser;
 	
 	public BukkitCommandWrapper(CommandData data, CommanderCommand command)  {
 		super(data.getName());
 		this.command = command;
+		this.parser = new CommandParser();
 	}
 
 	@Override
@@ -32,7 +36,12 @@ public class BukkitCommandWrapper extends Command {
 			commanderSender = new BukkitCommanderSender(bukkitSender);
 		}
 		
-		this.command.doCommand(commanderSender, commandLabel, new CommandArgs(Arrays.asList(args)));
+		ParserData data = this.parser.parse(this.command, args);
+		CommanderCommand commandToExecute = data.getCommand();
+		String[] executeArgs = data.getArgs();
+		
+		CommandArgs commandArgs = new CommandArgs(Arrays.asList(executeArgs));
+		commandToExecute.doCommand(commanderSender, commandLabel, commandArgs);
 		return true;
 	}
 }
